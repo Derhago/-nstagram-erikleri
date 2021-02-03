985c1fe4cc9f37860245952b0328aa4d73fd10ec
YazılımaKodluyorum <75515040+yazilimakodluyorum@users.noreply.github.com>
2021-02-03 21:21:32 +0300

    Add files via upload

---
+++ seviyoz.py
@@ -0,0 +1,16 @@
+import turtle
+
+turtle.bgcolor("black")
+t = turtle.Turtle()
+turtle.speed(1)
+t.color("white")
+t.begin_fill()
+t.fillcolor("red")
+t.left(140)
+t.forward(180)
+t.circle(-90,200)
+t.setheading(60)
+t.circle(-90,200)
+t.forward(180)
+t.end_fill()
+t.write("          Seviyoz İşte","true",'left',font=('open sans',15))
